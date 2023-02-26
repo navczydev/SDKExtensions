f3a1ce30a9d51165b9dabf62e128c4ea574cd673
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2023-02-26 16:38:25 -0500

    Added final composables DONE!!

--- app/src/main/java/com/navgde/sdkextensions/MainActivity.kt
+++ app/src/main/java/com/navgde/sdkextensions/MainActivity.kt
@@ -7,14 +7,21 @@ import android.os.Bundle
 import android.os.ext.SdkExtensions
 import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
@@ -26,10 +33,15 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.navgde.sdkextensions.ui.theme.SDKExtensionsTheme
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -74,6 +86,50 @@ fun PhotoPickerResultComposable(photoIntent: Intent) {
             text = "\uD83C\uDF05 SDKExtensions Sample \uD83C\uDF05",
             style = TextStyle(fontWeight = FontWeight.ExtraBold, fontSize = 28.sp),
         )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = "Android 11\n\nSDKExtensions version 4(R4)",
+            style = TextStyle(fontWeight = FontWeight.ExtraBold, fontSize = 28.sp),
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_android_black_24dp),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(100.dp),
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OpenPhotoPicker(
+            openLauncher = {
+                when {
+                    isPhotoPickerAvailable() -> launcher.launch(photoIntent)
+                    else -> Toast.makeText(context, "Photo picker not available", Toast.LENGTH_SHORT).show()
+                }
+            },
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        AsyncImage(
+            model = result,
+            contentDescription = "Image from photo picker",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(200.dp, 200.dp)
+                .clip(CircleShape),
+        )
+
+        Spacer(modifier = Modifier.height(48.dp))
+
+        Row {
+            Text(
+                "By Nav Singh Android GDE",
+                style = TextStyle(fontWeight = FontWeight.ExtraBold, fontSize = 28.sp),
+            )
+        }
     }
 }
 
